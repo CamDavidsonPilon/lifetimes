cdee1a6615d0f449d6ef26f010850ba5d105827e
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-02-09 23:29:41 -0500

    this is more efficient

--- lifetimes/generate_data.py
+++ lifetimes/generate_data.py
@@ -36,7 +36,7 @@ def beta_geometric_nbd_model(T, r, alpha, a, b, size=1):
     for i in xrange(size):
         p = probability_of_post_purchase_death[i]
         l = lambda_[i]
-        churn = np.argmax(stats.binom.rvs(1, p, size=10 / p)) + 1
+        churn = stats.geom.rvs(p)
         times = np.cumsum(np.r_[[0], stats.expon.rvs(scale=1. / l, size=churn)])
         t_cal = times[times < T[i]]
         df.ix[i] = len(t_cal) - 1,  max(t_cal), T[i], l, p, churn > len(t_cal) - 1, i
