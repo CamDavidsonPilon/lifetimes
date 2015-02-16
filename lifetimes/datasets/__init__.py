66053a36fed0739d4956d846858791f579dbc5ba
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-02-15 21:46:20 -0500

    adding better support for loading datasets

---
+++ lifetimes/datasets/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# modified from https://github.com/CamDavidsonPilon/lifelines/
+
+import pandas as pd
+from pkg_resources import resource_filename
+
+
+
+def load_dataset(filename, **kwargs):
+    '''
+    Load a dataset from lifetimes.datasets
+
+    Parameters:
+    filename : for example "larynx.csv"
+    usecols : list of columns in file to use
+
+    Returns : Pandas dataframe
+    '''
+    return pd.read_csv(resource_filename('lifetimes','datasets/' + filename),**kwargs)
+
+
+def load_cdnow(**kwargs):
+    return load_dataset('cdnow_customers.csv', **kwargs)
