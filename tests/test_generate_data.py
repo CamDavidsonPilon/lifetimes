a8c3ae965f2a5f05bf7bfaa55669dcf8b686c59b
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-04-27 18:12:10 -0400

    fixing impor

--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -3,14 +3,14 @@ import pandas as pd
 import numpy as np
 import scipy.stats as stats
 
-from lifetimes.generate_data import beta_geometric_nbd_model, beta_geometric_nbd_model2
+from lifetimes.generate_data import beta_geometric_nbd_model
 
 
 def test_statistics_of_beta_geometric_nbd_model():
     # This test is terrible, and should be based on actual statistics.
     N = 10000
     r, alpha, a, b = 0.11, 0.05, 1.41, 0.56
-    df = beta_geometric_nbd_model2(350*np.ones(N), r, alpha, a, b, N)
+    df = beta_geometric_nbd_model(350*np.ones(N), r, alpha, a, b, N)
     assert abs(df['frequency'].mean() - 1.26) < 0.2
     assert abs(df['lambda'].mean() - 2.272629382453732) < 0.2
     assert abs(df['p'].mean() - 0.7159928181941454) < 0.1
