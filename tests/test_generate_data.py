6f06f7ae3c94682723e362ced70119f7f102a5b9
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2015-04-27 20:28:25 -0400

    remove this test. I need a smarter way to test it

--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -6,15 +6,4 @@ import scipy.stats as stats
 from lifetimes.generate_data import beta_geometric_nbd_model
 
 
-def test_statistics_of_beta_geometric_nbd_model():
-    # This test is terrible, and should be based on actual statistics.
-    N = 10000
-    r, alpha, a, b = 0.11, 0.05, 1.41, 0.56
-    df = beta_geometric_nbd_model(350*np.ones(N), r, alpha, a, b, N)
-    assert abs(df['frequency'].mean() - 1.26) < 0.2
-    assert abs(df['lambda'].mean() - 2.272629382453732) < 0.2
-    assert abs(df['p'].mean() - 0.7159928181941454) < 0.1
-    assert abs(df['alive'].mean() - 0.3845) < 0.05
-
-
 
