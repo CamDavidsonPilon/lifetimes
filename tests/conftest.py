fb39013b8264ec1463f6107dffa62e6f5cbf02b9
Cameron Davidson-Pilon <cam.davidson.pilon@gmail.com>
2016-10-11 16:19:39 -0400

    whoops change back to initial seed

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import numpy as np
 
 
 def pytest_runtest_setup(item):
-    seed = 868
+    seed = np.random.randint(1000)
     print("Seed used in np.random.seed(): %d" % seed)
     np.random.seed(seed)
