66f90883321447bb7a020f4ca51b6d03bbad05d9
Anton Protopopov <anton.protopopov@gmail.com>
2017-08-11 09:29:50 +0300

    tests for cumulative and incremental transactions plots

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,12 +1,15 @@
 import pytest
 
+import pandas as pd
 import matplotlib
 matplotlib.use('AGG') # use a non-interactive backend
 from matplotlib import pyplot as plt
 
+
 from lifetimes import plotting
 from lifetimes import BetaGeoFitter, ParetoNBDFitter, ModifiedBetaGeoFitter
-from lifetimes.datasets import load_cdnow_summary, load_transaction_data
+from lifetimes.datasets import (load_cdnow_summary, load_transaction_data,
+                                load_dataset)
 from lifetimes import utils
 
 bgf = BetaGeoFitter()
@@ -17,13 +20,13 @@ bgf.fit(cd_data['frequency'], cd_data['recency'], cd_data['T'], iterative_fittin
 @pytest.mark.plottest
 class TestPlotting():
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_period_transactions(self):
         plt.figure()
         plotting.plot_period_transactions(bgf)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_period_transactions_parento(self):
         pnbd = ParetoNBDFitter()
         pnbd.fit(cd_data['frequency'], cd_data['recency'], cd_data['T'], iterative_fitting=1)
@@ -32,7 +35,7 @@ class TestPlotting():
         plotting.plot_period_transactions(pnbd)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_period_transactions_mbgf(self):
         mbgf = ModifiedBetaGeoFitter()
         mbgf.fit(cd_data['frequency'], cd_data['recency'], cd_data['T'], iterative_fitting=1)
@@ -41,79 +44,79 @@ class TestPlotting():
         plotting.plot_period_transactions(mbgf)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_period_transactions_max_frequency(self):
         plt.figure()
         plotting.plot_period_transactions(bgf, max_frequency=12)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_period_transactions_labels(self):
         plt.figure()
         plotting.plot_period_transactions(bgf, label=['A', 'B'])
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_frequency_recency_matrix(self):
         plt.figure()
         plotting.plot_frequency_recency_matrix(bgf)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_frequency_recency_matrix_max_recency(self):
         plt.figure()
         plotting.plot_frequency_recency_matrix(bgf, max_recency=100)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_frequency_recency_matrix_max_frequency(self):
         plt.figure()
         plotting.plot_frequency_recency_matrix(bgf, max_frequency=100)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_frequency_recency_matrix_max_frequency_max_recency(self):
         plt.figure()
         plotting.plot_frequency_recency_matrix(bgf, max_frequency=100, max_recency=100)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_probability_alive_matrix(self):
         plt.figure()
         plotting.plot_probability_alive_matrix(bgf)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_probability_alive_matrix_max_frequency(self):
         plt.figure()
         plotting.plot_probability_alive_matrix(bgf, max_frequency=100)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_probability_alive_matrix_max_recency(self):
         plt.figure()
         plotting.plot_probability_alive_matrix(bgf, max_recency=100)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_probability_alive_matrix_max_frequency_max_recency(self):
         plt.figure()
         plotting.plot_probability_alive_matrix(bgf, max_frequency=100, max_recency=100)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_expected_repeat_purchases(self):
         plt.figure()
         plotting.plot_expected_repeat_purchases(bgf)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_expected_repeat_purchases_with_label(self):
         plt.figure()
         plotting.plot_expected_repeat_purchases(bgf, label='test label')
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_customer_alive_history(self):
         plt.figure()
         transaction_data = load_transaction_data()
@@ -124,7 +127,7 @@ class TestPlotting():
         plotting.plot_history_alive(bgf, days_since_birth, sp_trans, 'date')
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_calibration_purchases_vs_holdout_purchases(self):
         transaction_data = load_transaction_data()
         summary = utils.calibration_and_holdout_data(transaction_data, 'id', 'date', '2014-09-01', '2014-12-31')
@@ -134,7 +137,7 @@ class TestPlotting():
         plotting.plot_calibration_purchases_vs_holdout_purchases(bgf, summary)
         return plt.gcf()
 
-    @pytest.mark.mpl_image_compare(tolerance=30)
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
     def test_plot_calibration_purchases_vs_holdout_purchases_time_since_last_purchase(self):
         transaction_data = load_transaction_data()
         summary = utils.calibration_and_holdout_data(transaction_data, 'id', 'date', '2014-09-01', '2014-12-31')
@@ -142,4 +145,48 @@ class TestPlotting():
 
         plt.figure()
         plotting.plot_calibration_purchases_vs_holdout_purchases(bgf, summary, kind='time_since_last_purchase')
+        return plt.gcf()
+
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
+    def test_plot_cumulative_transactions(self):
+        transactions = load_dataset('CDNOW_sample.txt', header=None, sep='\s+')
+        transactions.columns = ['id_total', 'id_sample', 'date', 'num_cd_purc',
+                                'total_value']
+        t = 39
+        freq = 'W'
+
+        transactions_summary = utils.summary_data_from_transaction_data(
+            transactions, 'id_sample', 'date', datetime_format='%Y%m%d',
+            observation_period_end='19970930', freq=freq)
+
+        bgf = BetaGeoFitter(penalizer_coef=0.01)
+        bgf.fit(transactions_summary['frequency'],
+                transactions_summary['recency'], transactions_summary['T'])
+
+        plt.figure()
+        plotting.plot_cumulative_transactions(
+            bgf, transactions, 'date', 'id_sample', 2 * t, t, freq=freq,
+            xlabel='week', datetime_format='%Y%m%d')
+        return plt.gcf()
+
+    @pytest.mark.mpl_image_compare(tolerance=2, style='default')
+    def test_plot_incremental_transactions(self):
+        transactions = load_dataset('CDNOW_sample.txt', header=None, sep='\s+')
+        transactions.columns = ['id_total', 'id_sample', 'date', 'num_cd_purc',
+                                'total_value']
+        t = 39
+        freq = 'W'
+
+        transactions_summary = utils.summary_data_from_transaction_data(
+            transactions, 'id_sample', 'date', datetime_format='%Y%m%d',
+            observation_period_end='19970930', freq=freq)
+
+        bgf = BetaGeoFitter(penalizer_coef=0.01)
+        bgf.fit(transactions_summary['frequency'],
+                transactions_summary['recency'], transactions_summary['T'])
+
+        plt.figure()
+        plotting.plot_incremental_transactions(
+            bgf, transactions, 'date', 'id_sample', 2 * t, t, freq=freq,
+            xlabel='week', datetime_format='%Y%m%d')
         return plt.gcf()
