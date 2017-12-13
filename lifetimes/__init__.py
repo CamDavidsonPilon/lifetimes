656a1a2eddcbd662bcfc393f02bb3cd7b02af789
Federico Bertola <fb@bendingspoons.com>
2017-12-13 09:53:36 +0100

    fixed version import

--- lifetimes/__init__.py
+++ lifetimes/__init__.py
@@ -1,4 +1,5 @@
 # from estimation import BetaGeoFitter, ParetoNBDFitter, GammaGammaFitter, ModifiedBetaGeoFitter
+from __future__ import absolute_import
 from .version import __version__
 
 # __all__ = ['BetaGeoFitter', 'ParetoNBDFitter', 'GammaGammaFitter', 'ModifiedBetaGeoFitter']
