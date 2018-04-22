de4941740791d60f3208e0f56eeb0964c8b01a6d
Zane Blanton <zeblanton@gmail.com>
2018-04-22 15:53:30 +0200

    Changed format for pytesting

--- tests/test_generate_data.py
+++ tests/test_generate_data.py
@@ -11,6 +11,10 @@ from lifetimes.generate_data import beta_geometric_nbd_model, pareto_nbd_model, 
     beta_geometric_beta_binom_model
 
 
+def setup_module(module):
+    np.random.seed(188898)
+
+
 class TestBetaGeoGeneration():
     params = [0.243, 4.414, 0.793, 2.426]
 
@@ -25,25 +29,24 @@ class TestModifiedBetaGeoNBDGeneration():
     expected = np.array([0.0078, 0.0532, 0.1506, 1.0405, 1.0437, 1.8576])
 
 
+bbgb_params = OrderedDict([('alpha', 1.204), ('beta', 0.750), ('gamma', 0.657),
+                           ('delta', 2.783)])
+
+
 class TestBetaGeoBetaBinomGeneration():
 
-    def __init__(self):
-        self.params = OrderedDict([('alpha', 1.204), ('beta', 0.750), ('gamma', 0.657),
-                                   ('delta', 2.783)])
-        np.random.seed(188898)
-
     def test_positivity(self):
-        sim_data = beta_geometric_beta_binom_model(N=6, **self.params, size=5000)
+        sim_data = beta_geometric_beta_binom_model(N=6, **bbgb_params, size=5000)
         assert (sim_data['frequency'] >= 0).all()
         assert (sim_data['recency'] >= 0).all()
 
     def test_hitting_max(self):
-        sim_data = beta_geometric_beta_binom_model(N=6, **self.params, size=5000)
+        sim_data = beta_geometric_beta_binom_model(N=6, **bbgb_params, size=5000)
         assert sim_data['frequency'].max() == 6
         assert sim_data['recency'].max() == 6
 
-    def test_alive_probs(self):     
-        sim_data = beta_geometric_beta_binom_model(N=6, **self.params, size=50000)
+    def test_alive_probs(self):
+        sim_data = beta_geometric_beta_binom_model(N=6, **bbgb_params, size=50000)
         assert (np.abs(sim_data.loc[(sim_data['frequency'] == 0) & (sim_data['recency'] == 0),
                                     'alive'].mean() - 0.11) < 0.01)
         assert (np.abs(sim_data.loc[(sim_data['frequency'] == 2) & (sim_data['recency'] == 4),
@@ -52,7 +55,7 @@ class TestBetaGeoBetaBinomGeneration():
                                     'alive'].mean() - 0.93) < 0.01)
 
     def test_params_same_from_sim_data(self):
-        sim_data = beta_geometric_beta_binom_model(N=6, **self.params, size=100000)
+        sim_data = beta_geometric_beta_binom_model(N=6, **bbgb_params, size=100000)
         bbtf = estimation.BetaGeoBetaBinomFitter()
         grouped_data = sim_data.groupby(['frequency', 'recency', 'n'])['customer_id'].count()
         grouped_data = grouped_data.reset_index().rename(columns={'customer_id': 'n_custs'})
@@ -60,5 +63,5 @@ class TestBetaGeoBetaBinomGeneration():
                  grouped_data['recency'],
                  grouped_data['n'],
                  grouped_data['n_custs'])
-        assert ((np.array(list(self.params.values())) - np.array(
+        assert ((np.array(list(bbgb_params.values())) - np.array(
             bbtf._unload_params('alpha', 'beta', 'gamma', 'delta'))) < 0.1).all()
