408e1af6754e59ca8f8b283ed7992c405fe669df
Harrison Ng <tplawyer@gmail.com>
2018-09-15 01:20:52 -0700

    revert to original hyp_term as negative hyp_term will not go through log

--- lifetimes/fitters/beta_geo_fitter.py
+++ lifetimes/fitters/beta_geo_fitter.py
@@ -179,16 +179,11 @@ class BetaGeoFitter(BaseFitter):
         _b = b + x
         _c = a + b + x - 1
         _z = t / (alpha + T + t)
-        ln_hyp_term = np.log(hyp2f1(_a, _b, _c, _z))
 
-        # if the value is inf, we are using a different but equivalent
-        # formula to compute the function evaluation.
-        ln_hyp_term_alt = np.log(hyp2f1(_c - _a, _c - _b, _c, _z)) + \
-            (_c - _a - _b) * np.log(1 - _z)
-        ln_hyp_term = where(np.isinf(ln_hyp_term), ln_hyp_term_alt, ln_hyp_term)
+        hyp_term = hyp2f1(_a, _b, _c, _z)
+
         first_term = (a + b + x - 1) / (a - 1)
-        second_term = (1 - exp(ln_hyp_term + (r + x) *
-                               np.log((alpha + T) / (alpha + t + T))))
+        second_term = (1 - hyp_term * ((alpha + T) / (alpha + t + T)) ** (r + x))
 
         numerator = first_term * second_term
         denominator = 1 + (x > 0) * (a / (b + x - 1)) * \
