d003322530385edad0ddffd5528c0bc7aae5bde4
Harrison Ng <tplawyer@gmail.com>
2018-09-15 11:41:11 -0700

    high frequency

--- tests/test_estimation.py
+++ tests/test_estimation.py
@@ -394,15 +394,15 @@ class TestBetaGeoFitter():
         actual = bfg.conditional_expected_number_of_purchases_up_to_time(t, x, t_x, T)
         assert abs(expected - actual) < 0.001
 
-    def test_conditional_expectation_overflow_error(self, cdnow_customers):
+    def test_conditional_expectation_overflow_error_with_high_frequency(self, cdnow_customers):
         bfg = estimation.BetaGeoFitter()
         bfg.params_ = OrderedDict([('r', 0.5458741247391189), ('alpha', 13.409316394557274), ('a', 0.0009994943799344323), ('b', 0.03899022143378801)])
         t = 180
-        x = 500
+        frequency = 500
         t_x = 0
         T = 5
         expected = 5.212
-        actual = bfg.conditional_expected_number_of_purchases_up_to_time(t, x, t_x, T)
+        actual = bfg.conditional_expected_number_of_purchases_up_to_time(t, frequency, t_x, T)
         assert abs(expected - actual) < 0.001
 
     def test_expectation_returns_same_value_Hardie_excel_sheet(self, cdnow_customers):
