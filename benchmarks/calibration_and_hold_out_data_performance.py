3cda10b18ff9a3bb4accecca5dd1aac4324654f0
Oren Shklarsky <orenshk@gmail.com>
2019-08-15 12:11:51 -0400

    run timeit on new calibration_and_holdout_data

---
+++ benchmarks/calibration_and_hold_out_data_performance.py
@@ -0,0 +1,21 @@
+"""
+Script demonstrating performance speed up on calibration_and_hold_out_data()
+"""
+import timeit
+from lifetimes.utils import calibration_and_holdout_data
+from lifetimes.datasets import load_transaction_data
+
+
+def run():
+    code = (
+        '''calibration_and_holdout_data(transaction_data, 'id', 'date',
+                                        calibration_period_end='2014-09-01',
+                                        observation_period_end='2014-12-31' )''')
+
+    print('running: {}'.format(code))
+    result = timeit.timeit(code, setup='transaction_data = load_transaction_data()', globals=globals(), number=1)
+    return result
+
+
+if __name__ == '__main__':
+    print(run())
